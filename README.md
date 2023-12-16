fb96252a5a2dc080e606b1106220b724cbed691a
Kimo Adel <kimodev1990@gmail.com>
2023-12-17 01:43:27 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ping-login-preview
-ping login website page responsive design
+# Frontend Mentor - Ping coming soon page
+
+![Design preview for the Ping coming soon page coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".container__loginData");
+const inputEmail = document.querySelector(".container__loginData__input");
+const errorLabel = document.querySelector(".container__label");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (inputEmail.value === "" || validateEmail(inputEmail.value) === true) {
+    showError();
+  } else {
+    hideError();
+  }
+});
+
+function validateEmail(inputText) {
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.match(mailFormat)) {
+    return false;
+  }
+  return true;
+}
+
+function showError() {
+  errorLabel.classList.remove("hidden");
+  inputEmail.classList.add("borderAlert");
+}
+
+function hideError() {
+  errorLabel.classList.add("hidden");
+  inputEmail.classList.remove("borderAlert");
+}
